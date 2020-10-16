70c312cff32450f419f27c8d01bcec012d542b93
Dorcy Jean Duke <dorcyd89@gmail.com>
2020-10-16 08:49:50 -0400

    Create authorization function here

---
+++ src/utils/authorization.js
@@ -0,0 +1,26 @@
+const { ForbiddenError } = require('apollo-server');
+const { combineResolvers, skip } = require('graphql-resolvers');
+
+const isAuthenticated = (parent, args, { me }) =>
+  me ? skip : new ForbiddenError('Not authenticated as user.');
+
+const isAdmin = combineResolvers(
+  isAuthenticated,
+  (parent, args, { me: { role } }) =>
+    role === 'ADMIN'
+    ? skip
+    : new ForbiddenError('Not authorized as admin.'),
+);
+
+// const isMessageOwner = async ( parent, { id }, { models, me } ) => {
+//     const myModel = await models.myModel.findById(id);
+
+//     if (myModel.userId != me.id) {
+//         throw new ForbiddenError('Not authenticated as owner.');
+//     }
+//     return skip;
+// };
+module.exports = {
+    isAuthenticated,
+    isAdmin
+}
