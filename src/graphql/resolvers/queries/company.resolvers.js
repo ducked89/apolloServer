44aa33638737443d10c46a71e0f0c98646124bde
Dorcy Jean Duke <dorcyd89@gmail.com>
2020-10-16 13:42:52 -0400

    Company queries

---
+++ src/graphql/resolvers/queries/company.resolvers.js
@@ -0,0 +1,25 @@
+const { combineResolvers } = require('graphql-resolvers');
+const { isAuthenticated } = require('../../../utils/authorization');
+
+module.exports = {
+    Query: {
+        companies: combineResolvers(
+            isAuthenticated,
+            async (parent, args, { models }, info) => {
+                return await models.Company.find({});
+            },
+        ),
+        company: async (parent, { id }, { models }) => {
+            return await models.Company.findById(id);
+        }
+    },
+    Company: {
+        author: async (company, args, { models }) => {
+            // return await loaders.user.load(company.author);
+            return await models.User.findOne({ _id: company.author});
+        },
+        users: async (company, args, { models }) => {
+            return await models.User.find({ company: company.id});
+        }
+    },
+};
